1a1e923f5d78b820bf788e0211d2a75ed98fdfad
Azka Farisa <azkafariza@yahoo.com>
2021-05-02 01:36:37 +0700

    category

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 
 // Import routes
 // const userRoutes = require("./routes/userRoutes")
+const categoryRoute = require("./routes/categoryRoute");
+const serviceRoute = require("./routes/serviceRoute");
 
 //Set body parser for HTTP post operation
 app.use(express.json());
@@ -23,6 +25,8 @@ require("./utils/associations");
 
 // app.use
 // app.use("/user", userRoutes)
+app.use("/category", categoryRoute);
+app.use("/service", serviceRoute);
 
 // Server running
 app.listen(3000, () => console.log("server running on port 3000"));
