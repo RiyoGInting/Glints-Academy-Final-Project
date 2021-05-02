f6668d9c3cc7b75deb099fe1e9562d8f256f268b
rahmat hidayat <rahmathidayat.imirius@gmail.com>
2021-05-02 13:25:38 +0700

    ADD sign partner

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,7 @@ const auth = require("../middlewares/auth/index");
 const router = express.Router();
 
 router.post("/signup", auth.signup, authController.getToken);
+router.post("/signup/partner", auth.signupPartner, authController.getTokenPartner);
 router.post("/signin", auth.signin, authController.getToken);
-
+router.post("/signin/partner", auth.signinPartner, authController.getTokenPartner);
 module.exports = router;
