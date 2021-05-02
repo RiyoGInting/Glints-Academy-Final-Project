81e05b1ff4c50e43d852e048f87e4e822627702c
riyo <riyoginting@gmail.com>
2021-05-02 17:04:05 +0700

    add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middlewares/auth")
 // import validators
 
 // import controllers
 const userController = require("../controllers/userController");
 
-router.get("/:id", userController.getOne);
+router.get("/:id", auth.adminOrUser ,userController.getOne);
 
 module.exports = router;
