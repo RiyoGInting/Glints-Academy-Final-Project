d126071140a945b518954d5a578f3dc06c1d4642
rahmat hidayat <rahmathidayat.imirius@gmail.com>
2021-05-04 01:05:04 +0700

    add security

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const fileUpload = require ("express-fileupload")
+
+//const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 
 // Import routes
@@ -19,6 +31,49 @@ app.use(
     extended: true,
   })
 );
+
+app.use(fileUpload())
+
+// Sanitize data
+//app.use(mongoSanitize());
+
+// Prevent XSS attact
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+// Use helmet
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// CORS
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  // create a write stream (in append mode)
+  let accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "access.log"),
+    {
+      flags: "a",
+    }
+  );
+
+  // setup the logger
+  app.use(morgan("combined", { stream: accessLogStream }));
+}
 
 app.use(express.static("public"));
 
