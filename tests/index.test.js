4b2206e61d7816ba304361b609fd97d1f0bbdd94
Ebit Neygo <ebit.neygo@gmail.com>
2021-06-03 23:16:18 +0700

    add blog Test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,9 @@
 const reviewTest = require("./review");
 const userTest = require("./user");
 const transactionTest = require("./transaction");
+const blogTest = require("./blog");
 
 describe("ReviewTest ", reviewTest);
 describe("User Test", userTest);
 describe("Transaction Test", transactionTest);
+describe("Blog Test", blogTest);
