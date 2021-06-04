ec9a8918b301eb925db3861dde147e2ef5aed122
ace <rahmathidayat.imirius@gmail.com>
2021-06-04 14:38:01 +0700

    review get all change

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,7 +3,7 @@ const userTest = require("./user");
 const transactionTest = require("./transaction");
 const blogTest = require("./blog");
 
-describe("ReviewTest ", reviewTest);
+//describe("ReviewTest ", reviewTest);
 describe("User Test", userTest);
-describe("Transaction Test", transactionTest);
-describe("Blog Test", blogTest);
+//describe("Transaction Test", transactionTest);
+//describe("Blog Test", blogTest);
